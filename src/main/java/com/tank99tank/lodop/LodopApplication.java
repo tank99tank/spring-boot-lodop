935dec504db50feb6463565531cdd1f69e2bb040
tank99tank <Pengbo@DESKTOP-MRT6LK0>
2020-11-11 15:07:41 +0800

    init

---
+++ src/main/java/com/tank99tank/lodop/LodopApplication.java
@@ -0,0 +1,13 @@
+package com.tank99tank.lodop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodopApplication.class, args);
+	}
+
+}
